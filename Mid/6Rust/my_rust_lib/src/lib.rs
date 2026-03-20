a650698963a162ee395d0697f24dd3ae1e9175a5
SpKillX <spkillx@yandex.ru>
2026-03-20 20:14:30 +0300

    add tests for task M6

--- Mid/6Rust/my_rust_lib/src/lib.rs
+++ Mid/6Rust/my_rust_lib/src/lib.rs
@@ -1,5 +1,5 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub fn add_numbers(a: i32, b: i32) -> i32 {
+    a + b
 }
 
 #[cfg(test)]
@@ -7,8 +7,17 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_positive_numbers() {
+        assert_eq!(add_numbers(10, 20), 30);
+    }
+
+    #[test]
+    fn test_negative_numbers() {
+        assert_eq!(add_numbers(-5, -5), -10);
+    }
+
+    #[test]
+    fn test_zero() {
+        assert_eq!(add_numbers(0, 100), 100);
     }
 }
