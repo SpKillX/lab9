67a0d9a56d925454bcac4b63787565e1aa766ab7
SpKillX <spkillx@yandex.ru>
2026-03-21 17:00:48 +0300

    final integration of go microservices and rust crypto library

--- Mid/6Rust/my_rust_lib/target/debug/deps/my_rust_lib-0931d2c233db9eb4.d
+++
@@ -1,7 +0,0 @@
-C:\Users\SpKill\Documents\GitHub\lab9\Mid\6Rust\my_rust_lib\target\debug\deps\my_rust_lib-0931d2c233db9eb4.d: src\lib.rs
-
-C:\Users\SpKill\Documents\GitHub\lab9\Mid\6Rust\my_rust_lib\target\debug\deps\libmy_rust_lib-0931d2c233db9eb4.rlib: src\lib.rs
-
-C:\Users\SpKill\Documents\GitHub\lab9\Mid\6Rust\my_rust_lib\target\debug\deps\libmy_rust_lib-0931d2c233db9eb4.rmeta: src\lib.rs
-
-src\lib.rs:
--- Mid/6Rust/my_rust_lib/target/debug/deps/my_rust_lib-c7cf22ce78c975f7.d
+++
@@ -1,5 +0,0 @@
-C:\Users\SpKill\Documents\GitHub\lab9\Mid\6Rust\my_rust_lib\target\debug\deps\my_rust_lib-c7cf22ce78c975f7.d: src\lib.rs
-
-C:\Users\SpKill\Documents\GitHub\lab9\Mid\6Rust\my_rust_lib\target\debug\deps\my_rust_lib-c7cf22ce78c975f7.exe: src\lib.rs
-
-src\lib.rs:
